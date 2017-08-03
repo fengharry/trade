ab58fbb7279151c3774e1434a26ce8050f2a3981
harry <fengharry126@126.com>
2017-08-03 22:02:14 +0800

    more

--- rise_check.py
+++ rise_check.py
@@ -10,24 +10,22 @@ from email.header import Header
 
 ADDR = 'smtp.qq.com'
 FROM = 'fengharry02@qq.com'
-TO = 'fengharry02@qq.com'
-server = smtplib.SMTP_SSL(ADDR)
+TO = 'fengharry02@126.com'
+
 TEST = 0;
 
 class stock_info:
     def __init__(self, a):
         self.stock_code = a; # 股票代码
         self.price = 0; # 比较价格
-        
-        self.message = MIMEText('Hello', 'plain', 'utf-8')
-        self.message['From'] = FROM
-        self.message['To'] = TO
         return
 
     def check_code(self):
         df = td.get_realtime_quotes(self.stock_code)
+        if((float(df['price']) == 0) or (float(df['pre_close']) == 0)): # 没开盘
+            return
         if(self.price == 0):
-            self.price = float(df['pre_close'])
+            self.price = float(df['price'])
         rise1 = 100 * (float(df['price']) - self.price) / self.price
         if(abs(rise1) > 0.5):
             self.price = float(df['price'])
@@ -39,14 +37,18 @@ class stock_info:
                 SUBJECT = 'buy ' + SUBJECT
             print(SUBJECT)
             if(TEST == 0):
-                self.message['Subject'] = Header(SUBJECT, 'utf-8')
-                server.sendmail(FROM, TO, self.message.as_string())
+                message = MIMEText('Hello', 'plain', 'utf-8')
+                message['From'] = FROM
+                message['To'] = TO
+                message['Subject'] = SUBJECT
+                server = smtplib.SMTP_SSL(ADDR)
+                server.ehlo(ADDR)
+                server.login(FROM, 'cltduxqlmbmndjhg') 
+                server.sendmail(FROM, TO, message.as_string())
+                server.quit()
         return
        
 if __name__ == "__main__":  
-    server.ehlo(ADDR)
-    server.login(FROM, 'cltduxqlmbmndjhg') 
-    
     a1 = stock_info('002422')
     a2 = stock_info('000536')
       
@@ -55,5 +57,3 @@ if __name__ == "__main__":
         a2.check_code()
         
         time.sleep(120)
-        
-    server.quit()
